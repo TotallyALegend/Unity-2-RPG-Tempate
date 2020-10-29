27ed02da768ce21920114e57728e32f2a8585456
TotallyALegend <68360107+TotallyALegend@users.noreply.github.com>
2020-10-28 20:03:56 -0700

    Worked on Saves and did NPC's Again

--- Assets/Main/Scripts/ScriptableObjectsCode/Items/Artifact.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Artifact", menuName = "CustomObject/Item/Artifact")]
-public class Artifact : Item
-{
-    readonly public ItemType itemType = ItemType.Artifact;
-}
--- Assets/Main/Scripts/ScriptableObjectsCode/Items/Misc.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Default", menuName = "CustomObject/Item/Default")]
-public class Misc : Item
-{
-    readonly public ItemType itemType = ItemType.Misc;
-}
